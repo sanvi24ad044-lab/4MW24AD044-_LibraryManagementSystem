0301f859948bf8417e37d4be45f2b37b1e83399d
sanvi <sanvi.24ad044@sode-edu.in>
2026-05-31 18:07:56 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function insertBook(){
+document.getElementById("result").innerHTML =
+"INSERT Operation Executed Successfully";
+}
+
+function updateBook(){
+document.getElementById("result").innerHTML =
+"UPDATE Operation Executed Successfully";
+}
+
+function deleteBook(){
+document.getElementById("result").innerHTML =
+"DELETE Operation Executed Successfully";
+}
+
+function displayBooks(){
+document.getElementById("result").innerHTML =
+"Displaying Library Records";
+}
+
+function executeProcedure(){
+document.getElementById("result").innerHTML =
+"Stored Procedure Executed Successfully";
+}
+
+function triggerDemo(){
+document.getElementById("result").innerHTML =
+"Trigger Executed Successfully";
+}
